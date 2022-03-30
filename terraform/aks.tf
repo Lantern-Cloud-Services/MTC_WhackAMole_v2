d15a2857d01bee560ca8cd75eee2bb60bd9240eb
jdvcDev <jason.cook@microsoft.com>
2022-03-29 21:17:21 -0600

    iterate

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -9,33 +9,33 @@ resource "azurerm_kubernetes_cluster" "default" {
   dns_prefix          = "${var.name}-aks-${var.environment}"
   depends_on          = [azurerm_role_assignment.default]
 
-  agent_pool_profile {
-    name            = "default"
-    count           = "${var.node_count}"
-    vm_size         = "${var.node_type}"
-    os_type         = "${var.node_os}"
-    os_disk_size_gb = 30
-    vnet_subnet_id  = "${azurerm_subnet.aks.id}"
-  }
+  #agent_pool_profile {
+  #  name            = "default"
+  #  count           = "${var.node_count}"
+  #  vm_size         = "${var.node_type}"
+  #  os_type         = "${var.node_os}"
+  #  os_disk_size_gb = 30
+  #  vnet_subnet_id  = "${azurerm_subnet.aks.id}"
+  #}
 
   service_principal {
     client_id     = "${azuread_application.default.application_id}"
     client_secret = "${azuread_service_principal_password.default.value}"
   }
 
-  role_based_access_control {
-    enabled = true
-  }
+  #role_based_access_control {
+  #  enabled = true
+  #}
 
   network_profile {
     network_plugin = "azure"
   }
 
-  addon_profile {
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = "${azurerm_log_analytics_workspace.default.id}"
-    }
+#  addon_profile {
+#    oms_agent {
+#      enabled                    = true
+#      log_analytics_workspace_id = "${azurerm_log_analytics_workspace.default.id}"
+#    }
 
     http_application_routing {
       enabled = true
